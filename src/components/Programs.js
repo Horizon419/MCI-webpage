f5113dc952d152a84a144959755692f4fe2b9e5f
JMAN <lambejohn6@gmail.com>
2022-10-10 14:42:37 +0100

    committed

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -1,33 +1,75 @@
 import { FaCrown } from 'react-icons/fa';
 import SectionHead from './SectionHead';
-import { programs } from '../data'
-import {Link} from 'react-router-dom'
-import Card from '../UI/Card';
+import { Link } from "react-router-dom";
 
 
 const Programs = () => {
   return (
-    <section className="programs">
-      <div className="container programs__container">
-        <SectionHead icon={<FaCrown />} title="Programs" />
+    <>
+      <section className="programs">
+        <div className="container programs__container">
+          <SectionHead icon={<FaCrown />} title="Our Programs" />
+        </div>
+      </section>
 
-        <div className="programs__wrapper">
-          {programs.map(({ id, icon, title, info }) => {
-            return (
-              <Card className="programs__programs" key={id}>
-                <span>{icon}</span>
-                <h4>{title}</h4>
-                <small>{info}</small>
-                <Link to='/contact' className="btn sm">
-                 More
-                  
-                </Link>
-              </Card>
-            );
-          })}
+      <div className="grid grid-cols-1 lg:grid-cols-4 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black">
+        <div className="bg-red-400 rounded-xl shadow-2xl">
+          <div className="p-8">
+            <h3 className="font-bold text-2xl my-6">Coding Academy</h3>
+            <p className="text-black-600 text-xl">
+              Click below to know more about this program
+            </p>
+          </div>
+          <div className="bg-slate-100 pl-8 py-4">
+            <p className="flex items-center text-indigo-600">
+              <Link to="/contact">View More</Link>
+            </p>
+          </div>
+        </div>
+        <div className="bg-red-400 rounded-xl shadow-2xl">
+          <div className="p-8">
+            <h3 className="font-bold text-2xl my-6">Fashion Designing</h3>
+            <p className="text-black-600 text-xl">
+              Click below to know more about this program
+            </p>
+          </div>
+          <div className="bg-slate-100 pl-8 py-4">
+            <p className="flex items-center text-indigo-600">
+              {" "}
+              <Link to="/contact">View More</Link>
+            </p>
+          </div>
+        </div>
+        <div className="bg-red-400 rounded-xl shadow-2xl">
+          <div className="p-8">
+            <h3 className="font-bold text-2xl my-6">Catering & Decoration</h3>
+            <p className="text-black-600 text-xl">
+              Click below to know more about this program
+            </p>
+          </div>
+          <div className="bg-slate-100 pl-8 py-4">
+            <p className="flex items-center text-indigo-600">
+              {" "}
+              <Link to="/contact">View More</Link>
+            </p>
+          </div>
+        </div>
+        <div className="bg-red-400 rounded-xl shadow-2xl">
+          <div className="p-8">
+            <h3 className="font-bold text-2xl my-6">MCI Driving School</h3>
+            <p className="text-black-600 text-xl">
+              Click below to know more about this program
+            </p>
+          </div>
+          <div className="bg-slate-100 pl-8 py-4">
+            <p className="flex items-center text-indigo-600">
+              {" "}
+              <Link to="/contact">View More</Link>
+            </p>
+          </div>
         </div>
       </div>
-    </section>
+    </>
   );
 }
 export default Programs
